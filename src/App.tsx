0f3b17ba8bfefe8cfc2fb073c4c3b7d8dbbabed8
klaymant <clement@rightsbridger.com>
2022-05-03 21:44:22 +0200

    Changed homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
-const App = () => <p>Nutrimate</p> 
+import { useState } from "react";
+import NavButton from "./component/NavButton";
+import Bmi from "./page/Bmi";
+
+const App = () => {
+  const [pageSelection, setPageSelection] = useState<'bmi'>('bmi');
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <div>
+          <nav className="mt-2 font-semibold">
+            <NavButton onClick={() => setPageSelection('bmi')}>BMI</NavButton>
+          </nav>
+
+          <section className="rounded p-4 m-4 bg-khaki">
+            {pageSelection === 'bmi' && <Bmi />}
+          </section>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
