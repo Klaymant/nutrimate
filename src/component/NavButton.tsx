adace69f47d08b9394d845bbce03f10a158a556d
klaymant <clement@rightsbridger.com>
2022-05-07 11:48:04 +0200

    Refacto of NavButton

--- src/component/NavButton.tsx
+++ src/component/NavButton.tsx
@@ -2,11 +2,14 @@ import { ReactNode } from "react";
 
 type NavButtonProps = {
   children: ReactNode;
+  active: boolean;
   onClick?: () => void;
 };
 
-const NavButton = ({ children, onClick }: NavButtonProps) => (
-  <button className="border-2 border-green-500 hover:bg-green-500 p-2" onClick={onClick}>{children}</button>
+const getActiveClasses = (active: boolean) => active ? 'border-green-500' : 'border-gray-600';
+
+const NavButton = ({ children, active, onClick }: NavButtonProps) => (
+  <button className={`border-b-4 ${getActiveClasses(active)} outline-none hover:text-green-500 p-2 m-2`} onClick={onClick}>{children}</button>
 );
 
 export default NavButton;
