df15a103b35cee5261e5cd1bdff14988fe0ac643
klaymant <clement@rightsbridger.com>
2022-05-10 20:18:59 +0200

    Redesign of NavButton

--- src/component/NavButton.tsx
+++ src/component/NavButton.tsx
@@ -6,10 +6,10 @@ type NavButtonProps = {
   onClick?: () => void;
 };
 
-const getActiveClasses = (active: boolean) => active ? 'border-green-500' : 'border-gray-600';
+const getActiveClasses = (active: boolean) => active ? 'border-mint-green' : 'border-gray-600';
 
 const NavButton = ({ children, active, onClick }: NavButtonProps) => (
-  <button className={`border-b-4 ${getActiveClasses(active)} outline-none hover:text-green-500 p-2 m-2`} onClick={onClick}>{children}</button>
+  <button className={`rounded-xl bg-mint-green ${getActiveClasses(active)} outline-none hover:text-white p-2 m-2 outline`} onClick={onClick}>{children}</button>
 );
 
 export default NavButton;
