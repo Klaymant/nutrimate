46bb9f64c2d9dbf62d3cb842c00dde28d472abb7
klaymant <clement@rightsbridger.com>
2022-05-10 20:19:19 +0200

    App redesign

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ const App = () => {
   return (
     <>
       <div className="flex justify-center">
-        <div>
+        <div className="w-full mx-4 sm:w-96">
           <nav className="mt-2 font-semibold">
             <NavButton onClick={() => setPageSelection('bmi')} active={pageSelection === 'bmi'}>BMI</NavButton>
             <NavButton onClick={() => setPageSelection('macro')} active={pageSelection === 'macro'}>Macro</NavButton>
           </nav>
 
-          <section className="rounded">
+          <section className="rounded-xl bg-cream-dark mt-4 p-4">
             {pageSelection === 'bmi' && <Bmi />}
             {pageSelection === 'macro' && <Macros />}
           </section>
