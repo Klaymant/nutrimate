2bc26e51f6275ed44aec060344d205e4dd59992a
klaymant <clement@rightsbridger.com>
2022-05-17 21:23:17 +0200

    Added effects to label on input's focus

--- src/component/Input.tsx
+++ src/component/Input.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, ReactNode, SetStateAction } from "react";
+import { Dispatch, ReactNode, SetStateAction, useState } from "react";
 
 type InputProps = {
   children: ReactNode;
@@ -8,15 +8,20 @@ type InputProps = {
 };
 
 const Input = ({ children, value, type = 'text', setValue }: InputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+
   const onChange = (event: any) => {
     setValue(event.target.value);
   };
 
+  const focus = () => setIsFocused(true);
+  const blur = () => setIsFocused(false);
+
   return (
     <>
       <div className="my-2">
-        <label className="mr-6 mb-1 block">{children}</label>
-        <input type={type} value={value} onChange={onChange} className="rounded border-2 border-grey-600 focus:border-mint-green pl-2 py-1 outline-none" />
+        <label className={`mr-6 mb-1 block ${isFocused ? 'text-cornflower-blue font-semibold ml-1 duration-300' : ''}`}>{children}</label>
+        <input type={type} value={value} onChange={onChange} onFocus={focus} onBlur={blur} className="rounded border-2 border-grey-600 focus:border-cornflower-blue pl-2 py-1 outline-none" />
       </div>
     </>
   )
