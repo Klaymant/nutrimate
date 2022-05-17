a4365dc7b1b604f22b02e90ce2129a0942f536ad
klaymant <clement@rightsbridger.com>
2022-05-17 21:24:39 +0200

    Added Header to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Header from "./component/Header";
 import NavButton from "./component/NavButton";
 import Bmi from "./page/Bmi";
 import Macros from "./page/Macros";
@@ -12,8 +13,11 @@ const App = () => {
         <div className="w-full mx-4 sm:w-96">
           <nav className="mt-2 font-semibold">
             <NavButton onClick={() => setPageSelection('bmi')} active={pageSelection === 'bmi'}>BMI</NavButton>
-            <NavButton onClick={() => setPageSelection('macro')} active={pageSelection === 'macro'}>Macro</NavButton>
+            <NavButton onClick={() => setPageSelection('macro')} active={pageSelection === 'macro'}>Macros</NavButton>
           </nav>
+
+          {pageSelection === 'bmi' && <Header pageTitle="BMI" />}
+          {pageSelection === 'macro' && <Header pageTitle="Macronutrients" />}
 
           <section className="rounded-xl bg-cream-dark mt-4 p-4">
             {pageSelection === 'bmi' && <Bmi />}
