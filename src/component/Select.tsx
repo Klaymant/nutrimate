a2c382c99a05118dc0926ebd34b215caf2d33828
klaymant <clement@rightsbridger.com>
2022-05-18 23:27:50 +0200

    Refacto of Select component

--- src/component/Select.tsx
+++ src/component/Select.tsx
@@ -1,18 +1,37 @@
+import { Dispatch, ReactNode, SetStateAction } from "react";
+import useFocus from "../hook/UseFocus";
+
 export type Option = {
   label: string;
   value: string;
 };
 
-type Props = {
+type Props<T> = {
+  children: ReactNode;
   options: Array<Option>;
+  value: T;
+  setValue: Dispatch<SetStateAction<T>>;
 };
 
-const Select = ({ options }: Props) => (
-  <select>
-    {options.map((option) => (
-      <option value={option.value}>{option.label}</option>
-    ))}
-  </select>
-);
+const Select = <T,>({ children, options, value, setValue }: Props<T>) => {
+  const { isFocused, focus, blur } = useFocus();
+
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  }
+
+  return (
+    <>
+      <div className="my-2">
+        <label className={`mr-6 mb-1 block ${isFocused ? 'text-cornflower-blue font-semibold ml-1 duration-300' : ''}`}>{children}</label>
+        <select onChange={handleChange} onFocus={focus} onBlur={blur} className="rounded outline-none my-2 w-full">
+          {options.map((option) => (
+            <option value={option.value}>{option.label}</option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
+};
 
 export default Select;
