d52c313518d978d64e503b4c42c3fc51ddaef155
klaymant <clement@rightsbridger.com>
2022-05-18 23:28:53 +0200

    Refacto of Input with useFocus

--- src/component/Input.tsx
+++ src/component/Input.tsx
@@ -1,4 +1,5 @@
-import { Dispatch, ReactNode, SetStateAction, useState } from "react";
+import { Dispatch, ReactNode, SetStateAction } from "react";
+import useFocus from "../hook/UseFocus";
 
 type InputProps = {
   children: ReactNode;
@@ -8,20 +9,17 @@ type InputProps = {
 };
 
 const Input = ({ children, value, type = 'text', setValue }: InputProps) => {
-  const [isFocused, setIsFocused] = useState(false);
+  const { isFocused, focus, blur } = useFocus();
 
   const onChange = (event: any) => {
     setValue(event.target.value);
   };
 
-  const focus = () => setIsFocused(true);
-  const blur = () => setIsFocused(false);
-
   return (
     <>
       <div className="my-2">
         <label className={`mr-6 mb-1 block ${isFocused ? 'text-cornflower-blue font-semibold ml-1 duration-300' : ''}`}>{children}</label>
-        <input type={type} value={value} onChange={onChange} onFocus={focus} onBlur={blur} className="rounded border-2 border-grey-600 focus:border-cornflower-blue pl-2 py-1 outline-none" />
+        <input type={type} value={value} onChange={onChange} onFocus={focus} onBlur={blur} className="rounded border-2 border-grey-600 focus:border-cornflower-blue pl-2 py-1 outline-none w-full" />
       </div>
     </>
   )
