dac3271435db2fac54b818ffb9e4a45016b1bc7a
klaymant <clement@rightsbridger.com>
2022-05-18 23:28:04 +0200

    Refacto of MacrosResult

--- src/component/Macros/MacrosResult.tsx
+++ src/component/Macros/MacrosResult.tsx
@@ -6,12 +6,21 @@ type Props = {
 };
 
 const MacrosResult = ({ caloriesAmount, proteinsAmount, fatAmount, carbsAmount }: Props) => (
-  <div className="mt-14 mb-8">
-    Calories : {caloriesAmount}kcal
-    Proteins : {proteinsAmount}g
-    Fat : {fatAmount}g
-    Carbs : {carbsAmount}g
-  </div>
+  <section className="mt-8 mb-8 p-4 flex justify-evenly bg-cornflower-blue bg-opacity-70 rounded">
+    <div>
+      <div>Calories :</div>
+      <div>Proteins :</div>
+      <div>Fat :</div>
+      <div>Carbs :</div>
+    </div>
+
+    <div className="text-right">
+      <div>{caloriesAmount}kcal</div>
+      <div>{proteinsAmount}g</div>
+      <div>{fatAmount}g</div>
+      <div>{carbsAmount}g</div>
+    </div>
+  </section>
 );
 
 export default MacrosResult;
