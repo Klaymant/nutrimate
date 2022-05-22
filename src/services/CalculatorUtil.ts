18e72b9e0ab4647e426ead051bd8d04939c0f56f
klaymant <clement@rightsbridger.com>
2022-05-22 12:21:37 +0200

    Refacto of calories & proteins calculations

--- src/services/CalculatorUtil.ts
+++ src/services/CalculatorUtil.ts
@@ -1,29 +1,44 @@
-import { ActivityLevel, Gender } from "../types/generic";
+import { ActivityLevel, UserData } from "../types/generic";
 
-const ACTIVITY_LEVEL_FACTOR: Record<ActivityLevel, number> = {
-  'none': 1,
-  'low': 1.2,
-  'mid': 1.375,
-  'high': 1.55,
-  'very high': 1.725,
+const ACTIVITY_LEVEL_FACTOR: Record<ActivityLevel, { calories: number; proteins: number }> = {
+  'none': {
+    calories: 1,
+    proteins: 0.8,
+  },
+  'low': {
+    calories: 1.2,
+    proteins: 1,
+  },
+  'mid': {
+    calories: 1.375,
+    proteins: 1.2,
+  },
+  'high': {
+    calories: 1.55,
+    proteins: 1.35,
+  },
+  'very high': {
+    calories: 1.725,
+    proteins: 1.5,
+  },
 };
 
 export const CalculatorUtil = {
   getBmi(weight: number, height: number): number {
     return height && weight && Math.round(weight / (height / 100) ** 2);
   },
-  calculateCalories(gender: Gender, height: number, weight: number, age: number, activityLevel: ActivityLevel): number {
-    if (gender === 'male') {
-      return Math.round((88.362 + (13.397 * weight) + (4.799 * height ) - (5.677 * age)) * ACTIVITY_LEVEL_FACTOR[activityLevel]);
+  calculateCalories(userData: UserData): number {
+    if (userData.gender === 'male') {
+      return Math.round((88.362 + (13.397 * userData.weight) + (4.799 * userData.height ) - (5.677 * userData.age)) * ACTIVITY_LEVEL_FACTOR[userData.activityLevel].calories);
     }
 
-    return Math.round((447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)) * ACTIVITY_LEVEL_FACTOR[activityLevel]);
+    return Math.round((447.593 + (9.247 * userData.weight) + (3.098 * userData.height) - (4.330 * userData.age)) * ACTIVITY_LEVEL_FACTOR[userData.activityLevel].calories);
   },
-  calculateProteins(weight: number): number {
-    return weight * 1.5;
+  calculateProteins(weight: number, activityLevel: ActivityLevel): number {
+    return Math.round(weight * ACTIVITY_LEVEL_FACTOR[activityLevel].proteins);
   },
   calculateFat(weight: number): number {
-    return weight * 1;
+    return Math.round(weight * 1);
   },
   calculateCarbs(caloriesAmount: number, proteinsAmount: number, fatAmount: number): number {
     return Math.round((caloriesAmount - (4 * proteinsAmount) - (4 * fatAmount)) / 4);
