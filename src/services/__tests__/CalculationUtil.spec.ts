a455995af8badcc9dcb72baec4a95332fd02221d
klaymant <clement@rightsbridger.com>
2022-05-24 20:27:17 +0200

    Refacto of CalculationUtil unit tests with type add to it.each()

--- src/services/__tests__/CalculationUtil.spec.ts
+++ src/services/__tests__/CalculationUtil.spec.ts
@@ -3,25 +3,27 @@ import { CalculationUtil } from "../CalculationUtil";
 const { calculateBmi } = CalculationUtil;
 
 describe('Tests for BMI calculation', () => {
-  it.each(([
+  type BmiDataset = [weight: number, height: number, expectedBmi: number];
+
+  it.each<BmiDataset>([
     [62, 165, 23],
     [95, 178, 30],
     [130, 182, 39],
     [40, 150, 18],
-  ]))('should %ikg & %icm give a bmi of %i', (weight: number, height: number, bmi: number) => {
+  ])('should %ikg & %icm give a bmi of %i', (weight: number, height: number, expectedBmi: number) => {
     const result: number = calculateBmi(weight, height);
 
-    expect(bmi).toBe(result);
+    expect(expectedBmi).toBe(result);
   })
 
-  it.each([
+  it.each<BmiDataset>([
     [0, 0, 0],
     [0, 165, 0],
     [62, 0, 0],
-  ])('should give 0 if weight (%ikg) or height(%icm) is 0', (weight: number, height: number, bmi: number) => {
+  ])('should give a bmi of 0 if weight (%ikg) or height(%icm) is 0', (weight: number, height: number, expectedBmi: number) => {
     const result: number = calculateBmi(weight, height);
 
-    expect(bmi).toBe(result);
+    expect(expectedBmi).toBe(result);
   })
 });
 
