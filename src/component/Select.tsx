4f66036cf9d56fdea0b23d07888c9364cc8a714f
klaymant <clement@rightsbridger.com>
2023-04-21 21:05:54 +0200

    Added default value for Select

--- src/component/Select.tsx
+++ src/component/Select.tsx
@@ -9,7 +9,7 @@ export type Option = {
 type Props<T> = {
   children: ReactNode;
   options: Array<Option>;
-  value: T;
+  value: string;
   setValue: Dispatch<SetStateAction<T>>;
 };
 
@@ -26,7 +26,7 @@ const Select = <T,>({ children, options, value, setValue }: Props<T>) => {
         <label className={`mr-6 mb-1 block ${isFocused ? 'text-cornflower-blue font-semibold ml-1 duration-300' : ''}`}>{children}</label>
         <select onChange={handleChange} onFocus={focus} onBlur={blur} className="rounded outline-none my-2 w-full">
           {options.map((option) => (
-            <option key={option.value} value={option.value}>{option.label}</option>
+            <option key={option.value} value={option.value} selected={option.value === value}>{option.label}</option>
           ))}
         </select>
       </div>
