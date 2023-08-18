e17034ca5f561cbd9bb10ab64e91361969addea0
klaymant <clement.gauthier@protonmail.com>
2023-08-18 10:36:52 +0200

    Made Input component's label classes more readable

--- src/component/Input.tsx
+++ src/component/Input.tsx
@@ -1,16 +1,13 @@
 import { Dispatch, ReactNode, SetStateAction } from "react";
 import useFocus from "../hook/UseFocus";
 
-type InputProps = {
-  children: ReactNode;
-  value: string | number;
-  type?: 'text' | 'pwd' | 'number';
-  setValue: Dispatch<SetStateAction<string>> | Dispatch<SetStateAction<number>>;
-};
 
 const Input = ({ children, value, type = 'text', setValue }: InputProps) => {
   const { isFocused, focus, blur } = useFocus();
-
+  const baseClasses = 'mr-6 mb-1 block';
+  const focusClasses = isFocused ? 'text-cornflower-blue font-semibold ml-1 duration-300' : '';
+  const labelClasses = [baseClasses, focusClasses].join(' ');
+  
   const onChange = (event: any) => {
     setValue(event.target.value);
   };
@@ -18,11 +15,18 @@ const Input = ({ children, value, type = 'text', setValue }: InputProps) => {
   return (
     <>
       <div className="my-2">
-        <label className={`mr-6 mb-1 block ${isFocused ? 'text-cornflower-blue font-semibold ml-1 duration-300' : ''}`}>{children}</label>
+        <label className={labelClasses}>{children}</label>
         <input type={type} value={value} onChange={onChange} onFocus={focus} onBlur={blur} className="rounded border-2 border-grey-600 focus:border-cornflower-blue pl-2 py-1 outline-none w-full" />
       </div>
     </>
   )
+};
+
+type InputProps = {
+  children: ReactNode;
+  value: string | number;
+  type?: 'text' | 'pwd' | 'number';
+  setValue: Dispatch<SetStateAction<string>> | Dispatch<SetStateAction<number>>;
 };
 
 export default Input;
