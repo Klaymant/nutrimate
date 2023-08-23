0dda96ffe72868c389b4268c34c3a5a4a5685242
klaymant <clement.gauthier@protonmail.com>
2023-08-23 16:12:01 +0200

    Corrected very high activity level messafe

--- src/services/UserDataManager.ts
+++ src/services/UserDataManager.ts
@@ -5,7 +5,7 @@ export const ACTIVITY_LEVEL_MAPPER: { label: ActivityLevel; message: string }[] 
     { label: 'low', message: 'Low activity (1-2 days/week)' },
     { label: 'mid', message: 'Moderate activity (2-3 days/week)' },
     { label: 'high', message: 'High activity (3-4 days/week)', },
-    { label: 'very high', message: 'Very high activity (4+ days a weeks)' },
+    { label: 'very high', message: 'Very high activity (4+ days/week)' },
 ];
 
 export class UserDataManager {
