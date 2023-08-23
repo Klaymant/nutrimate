b156c0de5ab247af57d8538daa31e8ec08240fa7
klaymant <clement.gauthier@protonmail.com>
2023-08-23 17:02:21 +0200

    Changed macros result design: brought some background colors and gave them more space. Also added a title before.

--- src/component/Macros/MacrosResult.tsx
+++ src/component/Macros/MacrosResult.tsx
@@ -1,26 +1,36 @@
+const MacroResultElement = ({ label, result, unity, backgroundColor }: MacroResultElementProps) => {
+  const baseClasses = "flex justify-between rounded p-2 my-2";
+
+  return (
+    <div className={[baseClasses, backgroundColor].join(" ")}>
+      <div>{label}</div>
+      <div className="text-white">{result}{unity}</div>
+    </div>
+  );
+};
+
+const MacrosResult = ({ caloriesAmount, proteinsAmount, fatAmount, carbsAmount }: Props) => (
+  <section className="mt-8 mb-8">
+      <h2 className="text-2xl">Your estimated macros per day</h2>
+      <MacroResultElement label="Calories" result={caloriesAmount} unity="kcal" backgroundColor="bg-primary" />
+      <MacroResultElement label="Proteins" result={proteinsAmount} unity="g" backgroundColor="bg-red-500" />
+      <MacroResultElement label="Fat" result={fatAmount} unity="g" backgroundColor="bg-yellow-500" />
+      <MacroResultElement label="Carbs" result={carbsAmount} unity="g" backgroundColor="bg-pink-500" />
+  </section>
+);
+
+type MacroResultElementProps = {
+  label: string;
+  result: number;
+  unity: string;
+  backgroundColor: string;
+};
+
 type Props = {
   caloriesAmount: number;
   proteinsAmount: number;
   fatAmount: number;
   carbsAmount: number;
 };
-
-const MacrosResult = ({ caloriesAmount, proteinsAmount, fatAmount, carbsAmount }: Props) => (
-  <section className="mt-8 mb-8 p-4 flex justify-evenly bg-primary bg-opacity-70 rounded">
-    <div>
-      <div>Calories :</div>
-      <div>Proteins :</div>
-      <div>Fat :</div>
-      <div>Carbs :</div>
-    </div>
-
-    <div className="text-right">
-      <div>{caloriesAmount}kcal</div>
-      <div>{proteinsAmount}g</div>
-      <div>{fatAmount}g</div>
-      <div>{carbsAmount}g</div>
-    </div>
-  </section>
-);
 
 export default MacrosResult;
